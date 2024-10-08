48ad6a162ab44612d1cf0f26f6a3116a34f7696c
Alug <alufoliehorst@gmail.com>
2024-10-08 03:40:10 +0200

    actually look for wad files in the dsda iwad directory

--- dsda-doom-selector.sh
+++ dsda-doom-selector.sh
@@ -3,10 +3,28 @@
 # go in the script directory so it can scan for pwads there
 cd "$(dirname "$0")"
 
-# idk prob should actually scan in the dsda iwad directory or smth
-iwad_options=("doom.wad" "doom2.wad")
+# Scan for IWADs in dsda-path
+dsda_dir="/usr/local/share/games/doom" #thats where the iwads should be placed ideally per install instructions
+iwad_files=()
+
+for wad in "$dsda_dir"/*.wad; do
+  if [[ ! -e "$wad" ]]; then
+    break
+  fi
+
+  # Skip "dsda-doom.wad"
+  if [[ "$(basename "$wad")" != "dsda-doom.wad" ]]; then
+    iwad_files+=("$wad")
+  fi
+done
+
+if [ ${#iwad_files[@]} -eq 0 ]; then
+  echo "No valid IWAD files found in $dsda_dir. Exiting."
+  exit 1
+fi
+
 echo "Select an IWAD:"
-select iwad in "${iwad_options[@]}"; do
+select iwad in "${iwad_files[@]}"; do
   if [ -n "$iwad" ]; then
     break
   else
